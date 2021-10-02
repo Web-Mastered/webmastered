b3459dc4dc817a24233bfc512239666e336d4e95
Sidharth S <sidharth.s16@yahoo.com>
2021-10-03 00:28:08 +0100

    Update oauth_validator.py

--- portal/oauth_validator.py
+++ portal/oauth_validator.py
@@ -3,6 +3,6 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 
 class CustomOAuth2Validator(OAuth2Validator):
     def get_userinfo_claims(self, request):
-        claims = super().get_userinfo_claims()
+        claims = super().get_userinfo_claims(request)
         claims["name"] = str(request.user.first_name) + str(request.user.last_name)
         return claims
